6656fecd393f147e7953ad0d3ea2651ac6874d22
Lavinatolani <lavinatolani842@yahoo.in>
2017-02-08 17:16:30 -0800

    fragment commit

---
+++ app/src/main/java/com/lavi/hp/fragmentationdemo4/MainActivity.java
@@ -0,0 +1,21 @@
+package com.lavi.hp.fragmentationdemo4;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm=getSupportFragmentManager();
+        FragmentTransaction ft=fm.beginTransaction();
+        Fragment1 f1=new Fragment1();
+        ft.add(R.id.l1,f1);
+        ft.commit();
+    }
+}
